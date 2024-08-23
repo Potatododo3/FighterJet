583e24ec4e9fc30668dd2be6d4a5d9624401ade2
Potatododo3 <baragamer0303@gmail.com>
2024-08-23 18:00:01 +0300

    Initial commit

---
+++ src/main/java/com/potato/fighterjet/FighterJet.java
@@ -0,0 +1,17 @@
+package com.potato.fighterjet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FighterJet extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/potato/fighterjet/Utility.java
@@ -0,0 +1,5 @@
+package com.potato.fighterjet;
+
+public class Utility {
+
+}
